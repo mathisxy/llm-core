0446b1061a7b0782d5e73ea6d4ff801ccd951520
mathisxy <mathis.siebert@gmail.com>
2026-01-26 23:38:25 +0000

    going back to data repräsentation basis

--- llm_ir/tools.py
+++ llm_ir/tools.py
@@ -1,19 +1,8 @@
-from pydantic import BaseModel, Field
-from typing import Any, Callable, Literal, Union
+from pydantic import BaseModel
+from typing import Any
 
-class ToolBase(BaseModel):
+class Tool(BaseModel):
     name: str
     description: str
     input_schema: dict[str, Any]
-
-class MCPTool(ToolBase):
-    type: Literal['mcp'] = "mcp"
-    server_id: str
-
-class FunctionTool(ToolBase):
-    type: Literal['function'] = "function"
-    function: Callable[..., Any] = Field(exclude=True)
-
-
-Tool = Union[MCPTool, FunctionTool]
 
